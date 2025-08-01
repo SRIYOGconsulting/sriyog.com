d13e2d2b4b604f085aa3ba23471f641a8976beee
Youndhen <youndyt929@gmail.com>
2025-08-01 11:25:09 +0545

    github issues fix

--- src/app/slides/page.tsx
+++ src/app/slides/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Google Slides | SRIYOG Consulting",
+  title: "Google slides | SRIYOG Consulting",
 };
 
 export default function Slides() {
@@ -12,62 +12,62 @@ export default function Slides() {
     {
       id: 1,
       title: "BIRATINFO - An Intro",
-      image: "/assets/images/slides/biratinfo.png",
+      image: "/assets/images/slide/biratinfo.png",
       path: "https://docs.google.com/presentation/d/1BGGyPusJSPIc1rwmqqDeT6f33Ne3S3xL9eZvtrYv82A/",
     },
     {
       id: 2,
       title: "Digital Literacy",
-      image: "/assets/images/slides/digital.png",
+      image: "/assets/images/slide/digital.png",
       path: "https://docs.google.com/presentation/d/1_3BZyF6_xJ1N36Oz_wrktdJhJGYZJ77vuO5mtv9qlso/edit?usp=sharing",
     },
     {
       id: 3,
       title: "Cyber Security",
-      image: "/assets/images/slides/cyber.png",
+      image: "/assets/images/slide/cyber.png",
       path: "https://docs.google.com/presentation/d/1bSA3tCwOYk2XSHr8dYdddOcUCsmvlnIW6Qf93Pu4Cew/edit?usp=sharing",
     },
     {
       id: 4,
       title: "Social Media Marketing",
-      image: "/assets/images/slides/social.png",
+      image: "/assets/images/slide/social.png",
       path: "https://docs.google.com/presentation/d/1S0de1z_R_0ck08h3ETFlQeRk5c-SGMCGSHOzKsiQSd0/edit?usp=sharing",
     },
     {
       id: 5,
       title: "SRIYOG Concept Note",
-      image: "/assets/images/slides/sriyog.png",
+      image: "/assets/images/slide/sriyog.png",
       path: "https://docs.google.com/presentation/d/1SmlhrAF3oniK8A_HdVe9pmh6u4ezXs7zsDNEVeTdxMk/edit?usp=sharing",
     },
     {
       id: 6,
       title: "#TechFriday - Professional UI/UX",
-      image: "/assets/images/slides/uiux.png",
+      image: "/assets/images/slide/uiux.png",
       path: "https://docs.google.com/presentation/d/1v0CmwcqmrM7N4LOind06-qUH6H5r5jLxzhc4_hlE6nc/edit?usp=sharing",
     },
     {
       id: 7,
       title: "SRIYOG Consulting",
-      image: "/assets/images/slides/consulting.png",
+      image: "/assets/images/slide/consulting.png",
       path: "https://docs.google.com/presentation/d/1B0ngzfRQ0hLrfmr_P2WyIGIabEY5Z-ZPyC-4syQFjRM/edit?usp=sharing",
     },
     {
       id: 8,
       title: "SRIYOG | Shark Tank Pitching",
-      image: "/assets/images/slides/sharktank.png",
+      image: "/assets/images/slide/sharktank.png",
       path: "https://docs.google.com/presentation/d/1wN5_V3ZE14TuBbg3E2L1NFK1I4I-7pbGQGUdyrWT0_8/edit?usp=sharing",
     },
     {
       id:9,
       title: "Next.js - #TechFriday",
-      image: "/assets/images/slides/nextjs.png",
+      image: "/assets/images/slide/nextjs.png",
       path: "https://docs.google.com/presentation/d/1KEYHWsUkPuYfzrBygAdUL2_S4S_UZWRmrVjutgsz5WI/edit?usp=sharing",
     }
   ];
 
   return (
     <>
-      <Ribbon name="Google Slides" des="" />
+      <Ribbon name="Google slide" des="" />
       <section className="max-w-[1180px] mx-auto px-6 lg:px-0 mb-[45px] mt-[45px] w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {portfolio.map((item) => (
@@ -92,7 +92,7 @@ export default function Slides() {
     target="_blank"
     className=" px-1 sm:px-4 py-2 border border-[#055D59] text-[#055D59] hover:bg-[#055D59] hover:text-white w-[30%] sm:w-[40%] mx-auto text-center rounded-lg transition-all ease-in duration-100 text-sm"
   >
-    View Slides
+    View slides
   </Link>
 </div>
 
