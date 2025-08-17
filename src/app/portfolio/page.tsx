eab5f80f53fb04d53a001c3aa07ca0326528aa9f
Youndhen <youndyt929@gmail.com>
2025-08-17 13:45:26 +0545

    issues solved

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -145,31 +145,29 @@ export default function PortfolioPage() {
   return (
     <>
     <Ribbon name="Portfolio" des=""/>
-      <section className="max-w-[1180px] mx-auto px-6 lg:px-0 mb-[45px] mt-[45px] w-full flex flex-col sm:flex-row gap-4 justify-between flex-wrap ">
-        {portfolio.map((item) => (
-          <div
-            className=" w-full sm:max-w-[316px] h-[300px] flex flex-col gap-1 mb-6"
-            key={item.id}
-          >
-            <Image
-              src={item.image}
-              width={400}
-              height={400}
-              alt={item.title}
-              className="w-full rounded-[15px] h-full object-cover  "
-            />
-            <h1 className="text-center my-2">{item.title}</h1>
-
-            <Link
-              href={`${item.path}`}
-              target="_blank"
-              className="px-4 py-2 border border-[#055D59] text-[#055D59] hover:bg-[#055D59] hover:text-white w-[40%] mx-auto text-center rounded-lg transition-all ease-in duration-[100] "
-            >
-              Browse More
-            </Link>
-          </div>
-        ))}
-      </section>
+    <section className="max-w-[1180px] mx-auto px-4 sm:px-6 lg:px-0 mb-[45px] mt-[45px] w-full flex flex-wrap gap-4 justify-center sm:justify-between">
+  {portfolio.map((item) => (
+    <div
+      key={item.id}
+      className="w-full sm:w-[316px] h-auto sm:h-[300px] flex flex-col gap-1 mb-6"
+    >
+      <Image
+        src={item.image}
+        width={400}
+        height={400}
+        alt={item.title}
+        className="w-full rounded-[15px] h-[300px] sm:h-full object-cover"
+      />
+      <h1 className="text-center my-2">{item.title}</h1>
+      <Link
+        href={`trainings/${item.path}`}
+        className="px-2 py-1 border border-[#055D59] text-md text-[#055D59] hover:bg-[#055D59] hover:text-white w-[50%] sm:w-[50%] mx-auto text-center rounded-lg transition-all ease-in duration-[100]"
+      >
+        Browse More
+      </Link>
+    </div>
+  ))}
+</section>
     </>
   );
 }
