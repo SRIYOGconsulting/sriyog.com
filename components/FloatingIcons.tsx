0b5e342863fc1fd4b1b52140bdbad11f309c0503
Youndhen <youndyt929@gmail.com>
2025-08-17 22:07:49 +0545

    whats app button fixed

--- components/FloatingIcons.tsx
+++ components/FloatingIcons.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 export default function FloatingIcons() {
   return (
     <div className="fixed z-[9999] bottom-36 right-10 text-center">
-      <div className="items-center justify-center flex-col b-2 border-[#4b4b4b] flex gap-6">
+      <div className="items-center justify-center flex-col flex gap-6">
+        {/* WhatsApp */}
         <Link
           href="https://wa.me/9779852024365?text=Hi%2C%20I%27m%20contacting%20you%20through%20www.sriyog.com.%20"
           target="_blank"
           className="group flex flex-col items-center "
         >
-          <div className="relative h-[30px] w-[40px] transition-all duration-700 ease-in-out">
+          <div className="relative h-[30px] w-[40px] transition-all duration-700 ease-in-out hover:rotate-[120deg]">
             <Image
               src="/assets/icons/whatsapp-new.svg"
               fill
@@ -19,30 +20,22 @@ export default function FloatingIcons() {
               alt="WhatsApp"
             />
           </div>
-          <div className="max-h-0 overflow-hidden group-hover:max-h-[50px] transition-all duration-700 ease-in-out">
-            <p className="text-[14px] opacity-0 translate-y-3 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-700 ease-in-out">
-              SRIYOG
-            </p>
-          </div>
         </Link>
+
+        {/* Phone with continuous pulse animation */}
         <Link
-          href="https://wa.me/9779852024365?text=Hi%2C%20I%27m%20contacting%20you%20through%20www.sriyog.com.%20"
+          href="tel:+9779852025735"
           target="_blank"
           className="group flex flex-col items-center "
         >
-          <div className="relative h-[30px] w-[40px] transition-all duration-700 ease-in-out">
+          <div className="relative h-[30px] w-[40px] animate-pulse-scale">
             <Image
               src="/assets/icons/phone.svg"
               fill
               priority
               className="object-contain"
-              alt="WhatsApp"
+              alt="Phone"
             />
-          </div>
-          <div className="max-h-0 overflow-hidden group-hover:max-h-[50px] transition-all duration-700 ease-in-out">
-            <p className="text-[14px] opacity-0 translate-y-3 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-700 ease-in-out">
-              SRIYOG
-            </p>
           </div>
         </Link>
       </div>
