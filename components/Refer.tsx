1c118b84cc3d1b59945a811e0ee30e3ab5041865
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2025-12-25 19:01:56 +0200

    Added airtable api for contact form

--- components/Refer.tsx
+++ components/Refer.tsx
@@ -2,14 +2,30 @@
 import Ribbon from "@/components/Ribbon";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-
+type Form = {
+    fullName: string,
+    email: string,
+    organizationName: string,
+    designation: string,
+    phone: string,
+    additionalNotes: string,
+    clientName: string,
+    clientContact: string,
+    clientEmail: string,
+    clientPhone: string,
+    clientBusinessType: string,
+    reason: string,
+    relation: string,
+}
 
 
 export default function Refer() {
-  const [phoneOrganization, setPhoneOrganization] = useState<string>("");
-  const [personalPhone, setPersonPhone] = useState<string>("");
+  const [isSending,setIsSending] = useState<boolean>(false);
+  const [submitted,setSubmitted] = useState<boolean>(false);
+  const [isChecked1, setIsChecked1] = useState<boolean>(false);
+  const [isChecked2, setIsChecked2] = useState<boolean>(false);
   const [focusStates, setFocusStates] = useState({
     fullName: false,
     email: false,
@@ -22,15 +38,78 @@ export default function Refer() {
     clientBusinessType: false,
     reason: false,
   });
+  const [form, setForm] = useState<Form>({
+    fullName: "",
+    email: "",
+    organizationName: "",
+    designation: "",
+    phone: "",
+    additionalNotes: "",
+    clientName: "",
+    clientContact: "",
+    clientEmail: "",
+    clientPhone: "",
+    clientBusinessType: "",
+    reason: "",
+    relation: "",
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>)=>{
+    setForm({...form,[e.target.name]: e.target.value})
+  }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setIsSending(true);
+      try {
+        const res = await fetch("/api/refer-form", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form),
+        });
+  
+        const data = await res.json();
+  
+        if (!res.ok) {
+          console.error(data.error);
+          alert("Failed to submit form: " + data.error);
+        } else {
+          console.log("Form submitted successfully!");
+          setSubmitted(true)
+        }
+      } catch (err) {
+        console.error(err);
+        alert("An error occurred. Try again.");
+      }finally{
+        setIsSending(false);
+       
+      }
+    };
   const inputField =
     "w-full  mt-1 outline-none bg:[#FFFFFF] shadow-[#CBD0DB2E] shadow-xl p-2  border-[#EAEAEA] border-1 rounded mb-4";
 
   return (
     <>
       <Ribbon name="Refer a Client" des="" />
+      {submitted && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="text-white bg-[#055d59] rounded-lg p-5 w-[90%] max-w-md text-center shadow-xl">
+            <h2 className="text-xl font-semibold mb-3">
+              Sended Successfully
+            </h2>
+            <p className=" mb-5">
+              Thank you! Your referral has been sent successfully.
+            </p>
+            <button
+              onClick={() => setSubmitted(false)}
+              className="bg-[#055d59] text-white border cursor-pointer active:bg-gray-300 active:text-[#055d59] hover:bg-white hover:text-[#055d59] border-white px-5 py-0.5 rounded-md"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
       <section className=" p-4 max-w-[1180px] mx-auto">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row gap-4 items-start md:items-stretch align-center justify-center ">
             {/* Left Column */}
             <div className="flex-1">
@@ -39,10 +118,14 @@ export default function Refer() {
               <div>
                 <label>Your Full Name</label>
                 <input
+                  required
+                  pattern="^[A-Za-z ]+$"
                   type="text"
                   placeholder={
                     focusStates.fullName ? "" : "Enter your full name"
                   }
+                  name="fullName"
+                  onChange={handleChange}
                   onFocus={() =>
                     setFocusStates({ ...focusStates, fullName: true })
                   }
@@ -59,7 +142,11 @@ export default function Refer() {
               <div>
                 <label>Email</label>
                 <input
-                  type="text"
+                  required
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  inputMode="email"
                   placeholder={focusStates.email ? "" : "Enter email"}
                   onFocus={() =>
                     setFocusStates({ ...focusStates, email: true })
@@ -80,8 +167,8 @@ export default function Refer() {
                 </label>
                 <PhoneInput
                   country={"np"}
-                  value={phoneOrganization}
-                  onChange={(phone: string) => setPhoneOrganization(phone)}
+                  value={form.phone}
+                  onChange={(phone) => setForm({...form,phone: phone})}
                   inputProps={{
                     name: "phone",
                     required: true,
@@ -102,6 +189,10 @@ export default function Refer() {
               <div className="mt-4">
                 <label>Organization / Company Name</label>
                 <input
+                  required
+                  pattern="^[A-Za-z ]+$"
+                  name="organizationName"
+                  onChange={handleChange}
                   type="text"
                   placeholder={
                     focusStates.organizationName ? "" : "Enter your full name"
@@ -122,6 +213,9 @@ export default function Refer() {
               <div>
                 <label>Designation</label>
                 <input
+                  required
+                  name="designation"
+                  onChange={handleChange}
                   type="text"
                   placeholder={
                     focusStates.designation ? "" : "Enter your designation"
@@ -141,13 +235,14 @@ export default function Refer() {
 
               <div>
                 <label>Your Relationship to the Referred Client</label>
-                <select className={inputField}>
+                <select required className={inputField} name="relation" onChange={handleChange}>
                   <option value="">Please select an option</option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
+                  <option value="Vendor Company">Vendor Company</option>
+                  <option value="Client">Client</option>
+                  <option value="Office Employee">Office Employee</option>
+                  <option value="Agent">Agent</option>
+                  <option value="Business Developer">Business Developer</option>
+                  <option value="other">Other</option>
                 </select>
               </div>
             </div>
@@ -161,6 +256,10 @@ export default function Refer() {
               <div>
                 <label>Client Name</label>
                 <input
+                  required
+                  pattern="^[A-Za-z ]+$"
+                  name="clientName"
+                  onChange={handleChange}
                   type="text"
                   placeholder={
                     focusStates.clientName
@@ -183,7 +282,10 @@ export default function Refer() {
               <div>
                 <label>Client Contact Person (Optional)</label>
                 <input
+                  pattern="^[A-Za-z ]+$"
                   type="text"
+                  name="clientContact"
+                  onChange={handleChange}
                   placeholder={
                     focusStates.clientContact
                       ? ""
@@ -205,7 +307,11 @@ export default function Refer() {
               <div>
                 <label>Client Email</label>
                 <input
-                  type="text"
+                  required
+                  inputMode="email"
+                  name="clientEmail"
+                  onChange={handleChange}
+                  type="email"
                   placeholder={
                     focusStates.clientEmail ? "" : "Enter client's email"
                   }
@@ -228,10 +334,10 @@ export default function Refer() {
                 </label>
                 <PhoneInput
                   country={"np"}
-                  value={personalPhone}
-                  onChange={(phone: string) => setPersonPhone(phone)}
+                  value={form.clientPhone}
+                  onChange={(phone: string) => setForm({...form,clientPhone:phone})}
                   inputProps={{
-                    name: "phone",
+                    name: "clientPhone",
                     required: true,
                     autoFocus: false,
                   }}
@@ -250,6 +356,9 @@ export default function Refer() {
               <div className="mt-4">
                 <label>Client Industry / Business Type</label>
                 <input
+                  required
+                  name="clientBusinessType"
+                  onChange={handleChange}
                   type="text"
                   placeholder={
                     focusStates.clientBusinessType
@@ -274,24 +383,14 @@ export default function Refer() {
 
               <div className="">
                 <label>Why Are You Referring Them?</label>
-                <input
-                  type="text"
-                  placeholder={
-                    focusStates.reason
-                      ? ""
-                      : "Tell us why you are referring them — the more detail, the better."
-                  }
-                  onFocus={() =>
-                    setFocusStates({ ...focusStates, reason: true })
-                  }
-                  onBlur={(e) =>
-                    setFocusStates({
-                      ...focusStates,
-                      reason: e.target.value ? true : false,
-                    })
-                  }
-                  className={inputField}
-                />
+                <select required className={inputField} name="reason" onChange={handleChange}>
+                  <option value="">Please select an option</option>
+                  <option value="Expecting 10% Commission">Expecting 10% Commission</option>
+                  <option value="Friend">Friend</option>
+                  <option value="20% Startup Discount">20% Startup Discount</option>
+                  <option value="Marketing Training">Marketing Training</option>
+                  <option value="other">Other</option>
+                </select>
               </div>
             </div>
           </div>
@@ -302,6 +401,8 @@ export default function Refer() {
               Additional Notes <span className="italic">(Optional)</span>
             </label>
             <textarea
+              name="additionalNotes"
+              onChange={handleChange}
               placeholder={
                 focusStates.additionalNotes
                   ? ""
@@ -322,14 +423,14 @@ export default function Refer() {
 
           <div className="text-sm text-[#555555] mt-6 gap-1 flex flex-col">
             <div className="flex items-center gap-2">
-              <input type="checkbox" className="bg-[#555555]" />{" "}
+              <input type="checkbox"  required checked={isChecked1} onChange={(e) => setIsChecked1(e.target.checked)}  className="bg-[#555555] cursor-pointer" />{" "}
               <label>
                 I confirm I have the referred client&apos;s permission to share
                 their contact information.
               </label>
             </div>
             <div className="flex items-center gap-2">
-              <input type="checkbox" />{" "}
+              <input type="checkbox" className="cursor-pointer" required  checked={isChecked2} onChange={(e) => setIsChecked2(e.target.checked)} />{" "}
               <label>
                 I understand that submitting this referral does not guarantee
                 compensation unless specified in a formal agreement.
@@ -340,9 +441,9 @@ export default function Refer() {
           <div className="w-full flex items-center justify-center mt-10">
             <button
               type="submit"
-              className="py-1 px-6  mb-24 text-white font-[800] bg-[#383838] border-[#055D59]-1 rounded-sm "
+              className="py-1 cursor-pointer  active:bg-[#7a7a7a] hover:bg-[#464646] px-6  mb-24 text-white font-[800] bg-[#383838] border-[#055D59]-1 rounded-sm "
             >
-              Send Referal
+              {isSending ? "Sending..." : "Send Referal"}
             </button>
           </div>
         </form>
