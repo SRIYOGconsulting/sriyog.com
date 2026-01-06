9895cb207b4d093a9de06b95cd5507dac7a01602
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2026-01-06 10:48:06 +0200

    Regular Update

--- src/app/career/Career.tsx
+++ src/app/career/Career.tsx
@@ -1,11 +1,6 @@
 'use client'
-<<<<<<< HEAD
+
 import { countries } from "@/data/countries";
-=======
-import Ribbon from "@/components/Ribbon";
-import { countries } from "@/data/countries";
-import { Console } from "console";
->>>>>>> dev
 import { useEffect, useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
