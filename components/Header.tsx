f463ff1aa3138e3e7eb5ba0fa00ee813e4ce1541
PRACAS Upreti <100864380+pracasupreti@users.noreply.github.com>
2026-05-22 22:43:07 +0545

    discover disabled

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,11 +28,11 @@ useEffect(() => {
     <header className="w-full">
       <section className="bg-[#055d59] py-3 text-white">
         <div className="max-w-295 mx-auto flex flex-col md:flex-row items-center justify-center gap-3 px-3 text-sm">
-          <i className="text-center">
+          {/* <i className="text-center">
             Discover skilled professionals all over Nepal—right from one app.
-          </i>
+          </i> */}
 
-          <div className="flex gap-4">
+          {/* <div className="flex gap-4">
             <Link
               href="https://sriyog.app/"
               target="_blank"
@@ -47,7 +47,7 @@ useEffect(() => {
             >
               X
             </button>
-          </div>
+          </div> */}
         </div>
       </section>
     </header>
