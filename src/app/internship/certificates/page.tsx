7b61d2db1d9b6fe4b3839e7e74b1a5f092937a1e
PRACAS Upreti <100864380+pracasupreti@users.noreply.github.com>
2026-05-22 22:41:49 +0545

    certificates

--- src/app/internship/certificates/page.tsx
+++ src/app/internship/certificates/page.tsx
@@ -23,9 +23,21 @@ const Internship = () => {
       <section className="lg:w-full ">
         <div className="">
           <div className="lg:max-w-[1180px] mx-auto my-0 md:max-w-[920px] sm:max-w-[720px]  lg:px-12">
+            
             <div>
-              <iframe class="airtable-embed" src="https://airtable.com/embed/appcRLi3R3qIFyPIU/shrp1B6DNo8qsHQ4q?viewControls=on" frameborder="0" onmousewheel="" width="100%" height="3533" style="background: transparent; border: 1px solid #ccc;"></iframe>
-            </div>
+            <iframe
+              className="airtable-embed"
+              src="https://airtable.com/embed/appcRLi3R3qIFyPIU/shrp1B6DNo8qsHQ4q?viewControls=on"
+              frameBorder="0"
+              width="100%"
+              height="3533"
+              style={{
+                background: "transparent",
+                border: "1px solid #ccc",
+              }}
+            ></iframe>
+          </div>
+
           </div>
         </div>
       </section>
