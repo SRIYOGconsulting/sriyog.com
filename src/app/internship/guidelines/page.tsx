20d8ec0ea69cb2b4f777a16b8d7f9bf363a7b974
PRACAS Upreti <100864380+pracasupreti@users.noreply.github.com>
2026-06-18 21:26:31 +0545

    Updated Internship Gudelines

--- src/app/internship/guidelines/page.tsx
+++ src/app/internship/guidelines/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
 
       <Section title="Things to know before joining an internship program.">
         <ul>
+          <li> - This internship program is to shape your career and we think that we are moulding you to work for fortune 500 companies, being in a corner to the Silicon Valley in near future.</li>
+          <li> - This is truly a chain of sharing experience, guiding candidates beyod the books.</li>
           <li>- This is an unpaid internship opportunity where candidates can contribute on real time and live projects. </li>
           <li>- Internship opportunity is strictly  for candidates on 7th or 8th semester studying in any 4 year bachelor degree. </li>
           <li>- We offer internship opportunities for any streams like Management, IT, Science, Journalism and various other streams to gain practical experience. </li>
@@ -29,14 +31,13 @@ export default function Home() {
 
       <Section title="Meeting & Attendance">
         <ul>
-          <li>- Join all meetings at least <strong>5 minutes before</strong> your scheduled slot.</li>
+          <li>- Join all meetings at least <strong>5 minutes 1 seconds before</strong> the scheduled slot.</li>
           <li>- Maintain punctuality and regular attendance.</li>
-          <li>- Maintain punctuality and regular attendance.</li>
-          <li>- If you need leave or will be absent, inform via email 24 hours in advance with valid details explaining the reason.</li>
-          <li>- Do not shout, create noise or speak unless it is necessary. Make sure you are specific on your project. </li>
-          <li>- Make sure you know basics of Google Meet</li>
+          <li>- If you need leave or will be absent, inform via email 24 hours 1 minute in advance with valid details explaining the reason.</li>
+          <li>- Please maintain a quiet and professional environment by speaking only when necessary and keeping background sounds to almost 20–30 dB (decibel). Make sure you are specific on your project. </li>
+          <li>- Make sure you know basics operations of Google Meet</li>
           <li>- Mute your mic until unless you are requested to speak. </li>
-          <li>- Make sure to push your codes before leaving the session every time. </li>
+          <li>- Make sure to push your codes in GitHubbefore leaving the session everytime. </li>
         </ul>
 
         <p style={styles.note}>
@@ -48,10 +49,10 @@ export default function Home() {
 
       <Section title="Professional Identity">
         <ul>
-          <li> Make sure to manage your professional introduction so your professionalism will reflect in all communications with our visiting professors/ faculties/ placement assisting companies/ mentors and project owners. </li>
-          <li>Enhance the way of your presentation and make sure to prepare a note and present formally during meetings. </li>
-          <li>Use a professional headshot as your profile picture.</li>
-          <li>Your displayed name must match your real name as per your government-issued ID.</li>
+          <li> - Make sure to manage your professional introduction so your professionalism will reflect in all communications with our visiting professors/ faculties/ placement assisting companies/ mentors and project owners. </li>
+          <li>- Enhance the way of your presentation and make sure to prepare a note and present formally during meetings. </li>
+          <li>- Use a professional headshot as your profile picture.</li>
+          <li>- Your displayed name must match your real name as per your government-issued ID.</li>
         </ul>
       </Section>
 
@@ -130,7 +131,7 @@ export default function Home() {
       <Section title="LinkedIn & Professional Profiles">
         <p>
           - Update your LinkedIn profile according to the <strong>Job Description</strong>,
-          role, and organization mentioned in your Internship Offer Letter.
+          role, and organization mentioned as in the Internship opportunity in the LinkedIn Vacancy.
         </p>
       </Section>
 
@@ -152,7 +153,7 @@ export default function Home() {
 
 
       <Section title="Certification / Completion">
-        
+        <ul>
     <li>- You are eligible to request an internship certificate only after successfully completing 60, 90, or 180 working days of internship, corresponding to a minimum of 180, 270, or 540 working hours respectively.</li>
 
 <li>- Before requesting a certificate, you must submit all project reports, handover documents, project guidelines, login credentials, email access details, and any other resources related to the projects you have worked on.</li>
@@ -169,10 +170,27 @@ export default function Home() {
 <li>- We are happy to provide a Letter of Recommendation or professional reference in the future based on your performance, contribution, and successful completion of the internship.</li>
 
 <li>- After completing your internship, you can request for 3 months of extension as an Internship Coordinator.</li>
-
+</ul>
       </Section>
 
-      Apply Now : <a href="/internship/apply" style={styles.link}> Click Here </a>
+<Section title="Post Internship Programs">
+        <ul>
+          <li>
+            - Candidates successfully completing the internship program may be eligible for starting any startup program in cooperation of SRIYOG Consulting.
+          </li>
+          <li>
+            - Candidates rated 4 stars can be eligible for having a opportunity to get a franchising opportunity of our company anywhere in the globe as a 49% profit sharing partner.  
+          </li>
+          <li> - Candidates rated 3 stars can work as a frelancer / agent in future projects.</li>
+          <li> - Candidates rated below 2 stars can contribute as a volunteer/ guide for level 3 candidates.</li>
+          <li> - Candidates rated as 1 star are always welcome to join any further internship opportunity, can also get 1 to 1 mentorship.</li>
+        </ul>
+
+        Note : The rating is based on their performance, contribution, and overall behavior during the internship program. We respect everyone's talent, even 1 star counts us a lot and values far more than moon or mars. 
+      </Section>
+      
+      View Internship Certificates : <a href="/internship/certificates" style={styles.link}> Click Here </a> <br />
+      Apply For Internship : <a href="/internship" style={styles.link}> Click Here </a>
     </main>
   );
 }
---
+++ src/app/internship/page.tsx
@@ -0,0 +1,41 @@
+import Ribbon from "@/components/Ribbon";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Internship | SRIYOG Consulting",
+  description: "Explore internship opportunities at SRIYOG Consulting.",
+  openGraph: {
+    title: "Internship | SRIYOG Consulting",
+    description: "Explore internship opportunities at SRIYOG Consulting.",
+    url: "https://www.sriyog.com/internship",
+    images: [
+      {
+        url: "https://sriyog.com/og/default.jpg",
+        alt: "Internship"
+      }
+    ]
+  }
+};
+const Internship = () => {
+  return (
+    <>
+      <Ribbon name="Internship" des="" />
+      <section className="lg:w-full ">
+        <div className="">
+          <div className="lg:max-w-[1180px] mx-auto my-0 md:max-w-[920px] sm:max-w-[720px]  lg:px-12">
+            <div>
+              <iframe
+                className="airtable-embed"
+                src="https://airtable.com/embed/appcRLi3R3qIFyPIU/pagBvKYaiRmZLtsSn/form"
+                width="100%"
+                height="1600"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Internship;
